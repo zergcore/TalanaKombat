58e72227e506ee21191da5dabee5e3fe5ea1301f
Zaidibeth Ramos <zaidi.ramos@gmail.com>
2023-07-20 23:30:50 -0400

    Narrative for the fight
    - Optimized debugging code (now all of it is deleted)
    - Narrative added

--- battle_functions.py
+++ battle_functions.py
@@ -1,11 +1,41 @@
 from utils import get_punch_in_string, remove_empty_strings, validate_move, validate_punch
 
-energy_table = {
+ENERGY_TABLE = {
     1: {
-      "DSDP": 3, "SDK": 2, "P": 1, "K": 1
+      "DSDP": {
+        "energy":3,
+        "name": "Taladoken"
+      },
+      "SDK": {
+        "energy":2,
+        "name": "Remuyuken"
+      },
+      "P": {
+        "energy":1,
+        "name": "Punno"
+      },
+      "K": {
+        "energy":1,
+        "name": "Patada"
+      },
     },
     2: {
-      "SAK": 3, "ASAP": 2, "P": 1, "K": 1
+      "SAK": {
+        "energy":3,
+        "name": "Remuyuken"
+      },
+      "ASAP": {
+        "energy":2,
+        "name": "Taladoken"
+      },
+      "P": {
+        "energy":1,
+        "name": "Punno"
+      },
+      "K": {
+        "energy":1,
+        "name": "Patada"
+      },
     }
   }
 
@@ -40,8 +70,8 @@ def cal_priority(player1, player2):
     elif p2_quantity_moves > p1_quantity_moves:
       return 1
     else:
-      p1_quantity_punches = cal_total_punches(1,player1["movimientos"], player1["golpes"], energy_table)
-      p2_quantity_punches = cal_total_punches(2,player2["movimientos"], player2["golpes"], energy_table)
+      p1_quantity_punches = cal_total_punches(1,player1["movimientos"], player1["golpes"], ENERGY_TABLE)
+      p2_quantity_punches = cal_total_punches(2,player2["movimientos"], player2["golpes"], ENERGY_TABLE)
       if p1_quantity_punches > p2_quantity_punches:
         return 2
       else:
@@ -52,12 +82,38 @@ def cal_priority(player1, player2):
 def execute_move(player: int, move: str, punch: str, energy_table: dict):
   move_key = get_punch_in_string(str(move)+str(punch), energy_table[player].keys())
   if move_key:
-    expended_energy = energy_table.get(player).get(move_key) if energy_table.get(player).get(move_key) else 0
-    print(move_key, expended_energy)
+    expended_energy = energy_table.get(player).get(move_key, 0).get("energy", 0)
     return expended_energy
   else:
-    print("No punch")
     return 0
+
+
+#Fight Narrative
+def generate_narration(player, move, punch, energy_table):
+
+  if player == 1:
+    player_name = "Tonyn"
+    other_player = "Arnaldor"
+  else:
+    player_name = "Arnaldor"
+    other_player = "Tonyn"
+
+  move_key = get_punch_in_string(str(move)+str(punch), energy_table[player].keys())
+  if move_key:
+    move_name = energy_table.get(player).get(move_key, "").get("name", "")
+
+  if move_name == "Remuyuken":
+    narration = f" ➢ {player_name} conecta un {move_name}."
+  elif move_name == "Taladoken":
+    narration = f" ➢ {player_name} usa un {move_name}."
+  elif move_name == "Patada":
+    narration = f" ➢ {player_name} avanza y da una {move_name}."
+  elif move_name == "Punno":
+    narration = f" ➢ {player_name} le da un puñetazo al pobre {other_player}"
+  else:
+    narration = f" ➢ {player_name} se mueve."
+
+  return narration
 
 
 # Play
@@ -74,28 +130,23 @@ def fight_combat(player1, player2):
   p2_punches = player2["golpes"]
 
   index = 0
+  narration = []
 
   while p1_energy > 0 and p2_energy > 0:
-    print("Turno {}".format(index+1))
     if attacker == 1:
-      print("Player 1 against player 2")
-      p2_energy -= execute_move(1, validate_move(p1_moves[index]), validate_punch(p1_punches[index]), energy_table)
-      attacker = 2
+      p2_energy -= execute_move(1, validate_move(p1_moves[index]), validate_punch(p1_punches[index]), ENERGY_TABLE)
+      narration.append(generate_narration(1, validate_move(p1_moves[index]), validate_punch(p1_punches[index]), ENERGY_TABLE))
       if p2_energy <= 0:
         break
-      print("Player 2 against player 1")
-      p1_energy -= execute_move(2, validate_move(p2_moves[index]), validate_punch(p2_punches[index]), energy_table)
-      attacker = 1
-
+      p1_energy -= execute_move(2, validate_move(p2_moves[index]), validate_punch(p2_punches[index]), ENERGY_TABLE)
+      narration.append(generate_narration(2, validate_move(p2_moves[index]), validate_punch(p2_punches[index]), ENERGY_TABLE))
     else:
-      print("Player 2 against player 1")
-      p1_energy -= execute_move(2, validate_move(p2_moves[index]), validate_punch(p2_punches[index]), energy_table)
-      attacker = 1
+      p1_energy -= execute_move(2, validate_move(p2_moves[index]), validate_punch(p2_punches[index]), ENERGY_TABLE)
+      narration.append(generate_narration(2, validate_move(p2_moves[index]), validate_punch(p2_punches[index]), ENERGY_TABLE))
       if p1_energy <= 0:
         break
-      print("Player 1 against player 2")
-      p2_energy -= execute_move(1, validate_move(p1_moves[index]), validate_punch(p1_punches[index]), energy_table)
-      attacker = 2
+      p2_energy -= execute_move(1, validate_move(p1_moves[index]), validate_punch(p1_punches[index]), ENERGY_TABLE)
+      narration.append(generate_narration(1, validate_move(p1_moves[index]), validate_punch(p1_punches[index]), ENERGY_TABLE))
 
     index += 1
 
@@ -103,8 +154,10 @@ def fight_combat(player1, player2):
       break
 
   if p1_energy > p2_energy:
-    return "Tonyn Stallone Gana la pelea y aun le queda {} de energía.".format(p1_energy)
+    narration.append(" ➢ Tonyn Stallone Gana la pelea y aun le queda {} de energía.".format(p1_energy))
   elif p2_energy > p1_energy:
-    return "Arnaldor Shuatseneguer Gana la pelea y aun le queda {} de energía.".format(p2_energy)
+    narration.append(" ➢ Arnaldor Shuatseneguer Gana la pelea y aun le queda {} de energía.".format(p2_energy))
   else:
-    return "La pelea termina en empate."
+    narration.append(" ➢ La pelea termina en empate.")
+
+  return "\n".join(narration)
--- utils.py
+++ utils.py
@@ -1,10 +1,3 @@
-def validate_string(string):
-  array = ["W", "A", "D", "S"]
-  for character in string:
-    if character not in array:
-      return False
-  return True
-
 def remove_empty_strings(array):
   return [item for item in array if item]
 
@@ -12,21 +5,15 @@ def get_punch_in_string(string, array):
   for item in array:
     if item in string:
       return item
-  if string[len(string)-1] in ["P", "K"]:
-    return  string[len(string)-1]
-  else:
-    return ""
+  return string[len(string)-1] if string[len(string)-1] in ["P", "K"] else ""
+
+def validate_combination(string, valid_chars):
+  return all(char in valid_chars for char in string)
 
 def validate_move(string):
-  move = str("".join(list(filter(validate_string, string))))
-  if len(move) > 5:
-    return ""
-  else:
-    return move
+  move = str("".join([x for x in string if validate_combination(x,"WADS")]))
+  return move if len(move) <= 5 else ""
 
 def validate_punch(string):
-  if len(string) > 1:
-    return ""
-  else: return string
-
-
+  punch = str("".join([x for x in string if validate_combination(x,"PK")]))
+  return punch if len(punch) <= 1 else ""
