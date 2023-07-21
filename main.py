5542b8d666ab9ec6ecca78e3044ca715297366af
Zaidibeth Ramos <zaidi.ramos@gmail.com>
2023-07-20 23:28:38 -0400

    Data is coming from a JSON file
    - Added the code to read a json file
    - Added a JSON file

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
+import json
 from battle_functions import fight_combat
 
-# Ejemplo de uso:
-# combat_json = {
-#     "player1": {
-#       "movimientos": ["D", "DSD", "S", "DSD", "SD"],
-#       "golpes": ["K", "P", "", "K", "P"]
-#     },
-#     "player2": {
-#       "movimientos": ["SA", "SA", "SA", "ASA", "SA"],
-#       "golpes": ["K", "", "K", "P", "P"]
-#     }
-# }
-
-combat_json = {
-  "player1":{
-    "movimientos":["SDD", "DSD","SA", "DSD"],
-		"golpes":["K", "P", "K", "P"],
-		},
-	"player2":{
-		"movimientos":["DSD", "WSAW", "ASA", "", "ASA","SA"],
-		"golpes":["P", "K", "K", "K", "P", "k"]
-		}
-}
-
 if __name__ == "__main__":
+  # Reading combat_json from file combat.json
+  with open("combat.json") as f:
+    combat_json = json.load(f)
+
   result = fight_combat(combat_json["player1"], combat_json["player2"])
   print(result)
