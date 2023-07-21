a5120c2297f1bcf28b39b322f9f63dc0f6d33be9
Zaidibeth Ramos <zaidi.ramos@gmail.com>
2023-07-21 00:14:13 -0400

    Refactoring the code for better legibility

--- battle_functions.py
+++ battle_functions.py
@@ -39,6 +39,10 @@ ENERGY_TABLE = {
     }
   }
 
+PLAYER_NAMES = {
+    1: "Tonyn",
+    2: "Arnaldor",
+}
 
 # Calculate priority of the players
 def cal_total_buttons(moves, punches):
@@ -48,30 +52,35 @@ def cal_total_buttons(moves, punches):
 def cal_total_moves(moves):
   return len("".join(moves))
 
-def cal_total_punches(player, moves, punches, energy_table):
-  possible_punches = energy_table[player].keys()
+def cal_total_punches(player, moves, punches):
+  possible_punches = ENERGY_TABLE[player].keys()
   punches = list(zip(moves, punches))
   final_punches = [get_punch_in_string(move+punch, possible_punches) for move, punch in punches]
   final_punches = remove_empty_strings(final_punches)
   return len(final_punches)
 
 def cal_priority(player1, player2):
-  p1_buttons = cal_total_buttons(player1["movimientos"], player1["golpes"])
-  p2_buttons = cal_total_buttons(player2["movimientos"], player2["golpes"])
+  p1_moves = player1["movimientos"]
+  p2_moves = player2["movimientos"]
+  p1_punches = player1["golpes"]
+  p2_punches = player2["golpes"]
+
+  p1_buttons = cal_total_buttons(p1_moves, p1_punches)
+  p2_buttons = cal_total_buttons(p2_moves, p2_punches)
   if p1_buttons > p2_buttons:
     return 2
   elif p2_buttons > p1_buttons:
     return 1
   else:
-    p1_quantity_moves = cal_total_moves(player1["movimientos"])
-    p2_quantity_moves = cal_total_moves(player2["movimientos"])
+    p1_quantity_moves = cal_total_moves(p1_moves)
+    p2_quantity_moves = cal_total_moves(p2_moves)
     if p1_quantity_moves > p2_quantity_moves:
       return 2
     elif p2_quantity_moves > p1_quantity_moves:
       return 1
     else:
-      p1_quantity_punches = cal_total_punches(1,player1["movimientos"], player1["golpes"], ENERGY_TABLE)
-      p2_quantity_punches = cal_total_punches(2,player2["movimientos"], player2["golpes"], ENERGY_TABLE)
+      p1_quantity_punches = cal_total_punches(1, p1_moves, p1_punches)
+      p2_quantity_punches = cal_total_punches(2, p2_moves, p2_punches)
       if p1_quantity_punches > p2_quantity_punches:
         return 2
       else:
@@ -79,28 +88,24 @@ def cal_priority(player1, player2):
 
 
 # Execute movements and punches
-def execute_move(player: int, move: str, punch: str, energy_table: dict):
-  move_key = get_punch_in_string(str(move)+str(punch), energy_table[player].keys())
+def execute_move(player: int, move: str, punch: str):
+  move_key = get_punch_in_string(str(move)+str(punch), ENERGY_TABLE[player].keys())
   if move_key:
-    expended_energy = energy_table.get(player).get(move_key, 0).get("energy", 0)
+    expended_energy = ENERGY_TABLE.get(player).get(move_key, 0).get("energy", 0)
     return expended_energy
   else:
     return 0
 
 
 #Fight Narrative
-def generate_narration(player, move, punch, energy_table):
+def generate_narration(player, move, punch):
 
-  if player == 1:
-    player_name = "Tonyn"
-    other_player = "Arnaldor"
-  else:
-    player_name = "Arnaldor"
-    other_player = "Tonyn"
+  player_name = PLAYER_NAMES.get(player, "")
+  other_player = PLAYER_NAMES.get(2 if player == 1 else 1, "")
 
-  move_key = get_punch_in_string(str(move)+str(punch), energy_table[player].keys())
+  move_key = get_punch_in_string(str(move)+str(punch), ENERGY_TABLE[player].keys())
   if move_key:
-    move_name = energy_table.get(player).get(move_key, "").get("name", "")
+    move_name = ENERGY_TABLE.get(player).get(move_key, "").get("name", "")
 
   if move_name == "Remuyuken":
     narration = f" ➢ {player_name} conecta un {move_name}."
@@ -123,41 +128,41 @@ def fight_combat(player1, player2):
   p1_energy=6
   p2_energy=6
 
-  p1_moves = player1["movimientos"]
-  p2_moves = player2["movimientos"]
+  p1_moves = list(map(validate_move, player1["movimientos"]))
+  p2_moves = list(map(validate_move, player2["movimientos"]))
 
-  p1_punches = player1["golpes"]
-  p2_punches = player2["golpes"]
+  p1_punches =  list(map(validate_punch, player1["golpes"]))
+  p2_punches =  list(map(validate_punch, player2["golpes"]))
 
   index = 0
   narration = []
 
   while p1_energy > 0 and p2_energy > 0:
     if attacker == 1:
-      p2_energy -= execute_move(1, validate_move(p1_moves[index]), validate_punch(p1_punches[index]), ENERGY_TABLE)
-      narration.append(generate_narration(1, validate_move(p1_moves[index]), validate_punch(p1_punches[index]), ENERGY_TABLE))
+      p2_energy -= execute_move(1, p1_moves[index], p1_punches[index])
+      narration.append(generate_narration(1, p1_moves[index], p1_punches[index]))
       if p2_energy <= 0:
         break
-      p1_energy -= execute_move(2, validate_move(p2_moves[index]), validate_punch(p2_punches[index]), ENERGY_TABLE)
-      narration.append(generate_narration(2, validate_move(p2_moves[index]), validate_punch(p2_punches[index]), ENERGY_TABLE))
+      p1_energy -= execute_move(2, p2_moves[index], p2_punches[index])
+      narration.append(generate_narration(2, p2_moves[index], p2_punches[index]))
     else:
-      p1_energy -= execute_move(2, validate_move(p2_moves[index]), validate_punch(p2_punches[index]), ENERGY_TABLE)
-      narration.append(generate_narration(2, validate_move(p2_moves[index]), validate_punch(p2_punches[index]), ENERGY_TABLE))
+      p1_energy -= execute_move(2, p2_moves[index], p2_punches[index])
+      narration.append(generate_narration(2, p2_moves[index], p2_punches[index]))
       if p1_energy <= 0:
         break
-      p2_energy -= execute_move(1, validate_move(p1_moves[index]), validate_punch(p1_punches[index]), ENERGY_TABLE)
-      narration.append(generate_narration(1, validate_move(p1_moves[index]), validate_punch(p1_punches[index]), ENERGY_TABLE))
+      p2_energy -= execute_move(1, p1_moves[index], p1_punches[index])
+      narration.append(generate_narration(1, p1_moves[index], p1_punches[index]))
 
     index += 1
 
     if index >= min(len(p1_moves), len(p2_moves)):
       break
 
-  if p1_energy > p2_energy:
-    narration.append(" ➢ Tonyn Stallone Gana la pelea y aun le queda {} de energía.".format(p1_energy))
-  elif p2_energy > p1_energy:
-    narration.append(" ➢ Arnaldor Shuatseneguer Gana la pelea y aun le queda {} de energía.".format(p2_energy))
-  else:
-    narration.append(" ➢ La pelea termina en empate.")
+  if p1_energy > p2_energy: winner = 1
+  elif p2_energy > p1_energy: winner = 2
+  else: winner = None
+
+  if winner: narration.append(" ➢ ✅ {} Gana la pelea y aun le queda {} de energía.".format(PLAYER_NAMES[winner], p1_energy if p1_energy > p2_energy else p2_energy))
+  else: narration.append(" ➢ La pelea termina en empate.")
 
   return "\n".join(narration)
